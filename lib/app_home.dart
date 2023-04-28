839cfc07e2dfd9d03a9d3b32ba10a923be0a1969
denos <costelasdenis@gmail.com>
2023-04-28 20:13:26 +0300

    Added button to calculate the trip

--- lib/app_home.dart
+++ lib/app_home.dart
@@ -41,119 +41,136 @@ class AppHome extends StatelessWidget {
           const SizedBox(height: 10),
           Container(
             width: double.infinity,
-            height: 250,
+            height: 320,
             // color: Colors.red,
             padding: const EdgeInsets.all(20),
             child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: TextFormField(
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.number,
-                        autocorrect: false,
-                        textInputAction: TextInputAction.next,
-                        autofocus: true,
-                        decoration: const InputDecoration(
-                            hintText: 'Distanta',
-                            border: InputBorder.none,
-                            hintStyle: TextStyle()),
-                        onSaved: (distanta) {
-                          if (distanta != null) {
-                            _values['distanta'] = distanta;
-                          }
-                        },
-                        validator: (value) {
-                          if (value == null) {
-                            return 'Te rog introdu o valoare';
-                          }
-                          return null;
-                        },
-                      ),
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: TextFormField(
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                      autocorrect: false,
+                      textInputAction: TextInputAction.next,
+                      autofocus: true,
+                      decoration: const InputDecoration(
+                          hintText: 'Distanta',
+                          border: InputBorder.none,
+                          hintStyle: TextStyle()),
+                      onSaved: (distanta) {
+                        if (distanta != null) {
+                          _values['distanta'] = distanta;
+                        }
+                      },
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Te rog introdu o valoare';
+                        }
+                        return null;
+                      },
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: TextFormField(
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.number,
-                        autocorrect: false,
-                        textInputAction: TextInputAction.next,
-                        autofocus: true,
-                        decoration: const InputDecoration(
-                            hintText: 'Consum',
-                            border: InputBorder.none,
-                            hintStyle: TextStyle()),
-                        onSaved: (consum) {
-                          if (consum != null) {
-                            _values['consum'] = consum;
-                          }
-                        },
-                        validator: (value) {
-                          if (value == null) {
-                            return 'Te rog introdu o valoare';
-                          }
-                          return null;
-                        },
-                      ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: TextFormField(
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                      autocorrect: false,
+                      textInputAction: TextInputAction.next,
+                      autofocus: true,
+                      decoration: const InputDecoration(
+                          hintText: 'Consum',
+                          border: InputBorder.none,
+                          hintStyle: TextStyle()),
+                      onSaved: (consum) {
+                        if (consum != null) {
+                          _values['consum'] = consum;
+                        }
+                      },
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Te rog introdu o valoare';
+                        }
+                        return null;
+                      },
                     ),
-                    Container(
-                      // color: Colors.amber,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius: BorderRadius.circular(20)),
-                              child: TextFormField(
-                                textAlign: TextAlign.center,
-                                keyboardType: TextInputType.number,
-                                autocorrect: false,
-                                textInputAction: TextInputAction.next,
-                                autofocus: true,
-                                decoration: const InputDecoration(
-                                    hintText: 'Pret',
-                                    border: InputBorder.none,
-                                    hintStyle: TextStyle()),
-                                onSaved: (pret) {
-                                  if (pret != null) {
-                                    _values['pret'] = pret;
-                                  }
-                                },
-                                validator: (value) {
-                                  if (value == null) {
-                                    return 'Te rog introdu o valoare';
-                                  }
-                                  return null;
-                                },
-                              ),
+                  ),
+                  Container(
+                    // color: Colors.amber,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(20)),
+                            child: TextFormField(
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              autocorrect: false,
+                              textInputAction: TextInputAction.next,
+                              autofocus: true,
+                              decoration: const InputDecoration(
+                                  hintText: 'Pret',
+                                  border: InputBorder.none,
+                                  hintStyle: TextStyle()),
+                              onSaved: (pret) {
+                                if (pret != null) {
+                                  _values['pret'] = pret;
+                                }
+                              },
+                              validator: (value) {
+                                if (value == null) {
+                                  return 'Te rog introdu o valoare';
+                                }
+                                return null;
+                              },
                             ),
                           ),
-                          Container(
-                            height: 50,
-                            width: 50,
-                            // color: Colors.green,
-                            child: IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Icons.list,
-                                color: Colors.grey,
-                                size: 40,
-                              ),
+                        ),
+                        Container(
+                          height: 50,
+                          width: 50,
+                          // color: Colors.green,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.list,
+                              color: Colors.grey,
+                              size: 40,
                             ),
-                          )
-                        ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 40,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      child: const Text(
+                        'CALCULEAZA',
+                        style: TextStyle(color: Colors.white),
                       ),
                     ),
-                  ],
-                )),
+                  )
+                ],
+              ),
+            ),
           )
         ]),
       ),
